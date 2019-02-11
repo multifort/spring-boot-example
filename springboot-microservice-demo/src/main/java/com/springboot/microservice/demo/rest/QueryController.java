d857babd91b26aaab2767c951a18fc7b9c7525a3
multifort <fkdtz2008@gmail.com>
2019-02-11 18:00:04 +0800

    "Add queryController demo"

---
+++ springboot-microservice-demo/src/main/java/com/springboot/microservice/demo/rest/QueryController.java
@@ -0,0 +1,32 @@
+package com.springboot.microservice.demo.rest;
+
+import com.springboot.microservice.demo.log.annonation.AuditLog;
+import com.springboot.microservice.demo.service.QueryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@Slf4j
+@RequestMapping(value = "/query", produces = "application/json; charset=UTF-8")
+public class QueryController {
+
+    @Resource
+    private QueryService queryService;
+
+    @GetMapping(value = "/list")
+    @AuditLog
+    @ResponseBody
+    public String list(HttpServletRequest request){
+        return queryService.list();
+    }
+
+    @GetMapping(value = "/{name}")
+    @AuditLog
+    @ResponseBody
+    public String get(HttpServletRequest request, @PathVariable(value = "name")String name){
+        return queryService.get(name);
+    }
+}
